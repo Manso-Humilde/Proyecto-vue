c4ddd7e10438f567530186c08e3fc98c370ba1a1
Manso-Humilde <edgar.morel.fp@gmail.com>
2025-10-03 09:14:48 +0200

    V2

---
+++ vue-project/src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    router: [
+        {
+            path:'/',
+            name: 'home',
+            component: HomeView
+        },
+        {
+            path: 'about',
+            name: 'about',
+            //router level code-splitting
+            //this generates a separate chunk (About.[hash].js) for this route
+            //which is lazy-loaded when the router is visited.
+            component: () => import('../views/AboutView.vue')
+        }
+    ]
+})
