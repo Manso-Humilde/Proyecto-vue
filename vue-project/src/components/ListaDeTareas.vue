c7cc7d254d6c452d2d303d00664bbefe30519b20
Manso-Humilde <edgar.morel.fp@gmail.com>
2025-10-04 10:29:31 +0200

    crear segundo componente

---
+++ vue-project/src/components/ListaDeTareas.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>Lista de Tareas</h2>
+        <input type="text" v-model="nuevaTarea" @keyup.enter="addTarea">
+        <ul>
+            <li v-for="(tarea, index) in tarea" :key="index">
+                {{ tarea }}
+            </li>
+        </ul>
+    </div>
+</template>
+<script setup>
+import {ref} from 'vue';
+const tarea = ref([]);
+const nuevaTarea = ref('');
+const addTarea = () => {
+    tarea.value.push(nuevaTarea.value);
+    nuevaTarea.value = '';
+}
+</script>
+<style></style>
