db704d62da7d42989fa81b75bb905333e5ea7eb1
Manso-Humilde <edgar.morel.fp@gmail.com>
2025-10-06 08:05:09 +0200

    Crear Store de pinia para almacenar los datos de registro

--- vue-project/src/modules/registro/views/RegistrarView.vue
+++ vue-project/src/modules/registro/views/RegistrarView.vue
@@ -4,12 +4,12 @@
         <Form v-bind:validation-schema="schema" @submit="onSubmit">
           <div class="form">
             <label for="nombre">Nombre:</label>
-            <Field type="text" name="nombre" id="nombre" placeholder="ingrese su nombre" />
+            <Field v-model="nombre" type="text" name="nombre" id="nombre" placeholder="ingrese su nombre" />
             <ErrorMessage name="nombre"></ErrorMessage>
           </div>
           <div class="form">
             <label for="correo">Correo:</label>
-            <Field type="email" name="email" id="correo" placeholder="ingrese su email" />
+            <Field v-model="email" type="email" name="email" id="correo" placeholder="ingrese su email" />
             <ErrorMessage name="email"></ErrorMessage>
           </div>
           <div class="form">
@@ -23,8 +23,16 @@
 <script setup>
 import { Form, Field, ErrorMessage } from 'vee-validate';
 import {schema} from '../schemas/validationSchemas';
+import { usesRegistrarStore } from '../stores/registrarStore';
+import { ref } from 'vue';
+
+const registrarStore = usesRegistrarStore();
+
+const nombre = ref('');
+const email = ref('');
 
 const onSubmit = () => {
+  registrarStore.guardarRegistro(nombre.value, email.value)
   console.log('Se ha enviado el formulario');
 }
 </script>
