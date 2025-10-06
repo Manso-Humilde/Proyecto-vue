2038c2b0f52560a47bc92ec25d3629b3c5518cf1
Manso-Humilde <edgar.morel.fp@gmail.com>
2025-10-06 07:25:37 +0200

    Instala vee-validate. Crea Modulos

---
+++ vue-project/src/modules/registro/views/RegistrarView.vue
@@ -0,0 +1,36 @@
+<template>
+      <div>
+         <h2>Formulario de registro</h2>
+        <Form v-bind:validation-schema="schema" @submit="onSubmit">
+          <div class="form">
+            <label for="nombre">Nombre:</label>
+            <Field type="text" name="nombre" id="nombre" placeholder="ingrese su nombre" />
+            <ErrorMessage name="nombre"></ErrorMessage>
+          </div>
+          <div class="form">
+            <label for="correo">Correo:</label>
+            <Field type="email" name="email" id="correo" placeholder="ingrese su email" />
+            <ErrorMessage name="email"></ErrorMessage>
+          </div>
+          <div class="form">
+            <button type="submit">Registrar</button>
+          </div>
+        </Form>
+    </div>
+
+</template>
+  
+<script setup>
+import { Form, Field, ErrorMessage } from 'vee-validate';
+import {schema} from '../schemas/validationSchemas';
+
+const onSubmit = () => {
+  console.log('Se ha enviado el formulario');
+}
+</script>
+
+<style scoped>
+.form {
+  margin-bottom: 10px;
+}
+</style>
